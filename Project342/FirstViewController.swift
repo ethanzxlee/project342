f5579de6607a81190fec108092ded117837fb617
Zhe Xian Lee <zxlee618@gmail.com>
2016-05-15 18:44:39 +1000

    Initial Commit

---
+++ Project342/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Project342
+//
+//  Created by Zhe Xian Lee on 15/05/2016.
+//  Copyright © 2016 UOW. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
