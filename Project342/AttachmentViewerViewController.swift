934f26fc374be8b7cf2f3ce1d4ea179bcf583681
Fagan21 <keong07155@hotmail.com>
2016-05-21 16:44:51 +1000

    Attachment View: ScrollView with ImgView work propertly

--- Project342/AttachmentViewerViewController.swift
+++ Project342/AttachmentViewerViewController.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 
-class AttachmentViewerViewController: UIViewController {
+class AttachmentViewerViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBOutlet weak var imgViewTopConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var imgViewLeadingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var imgViewTrailingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var imgViewBottomConstraint: NSLayoutConstraint!
+    
+    var attachment : Attachment?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imgView.image = UIImage(named: "pic.png")!
+        imgView.contentMode = .ScaleAspectFit
+        scrollView.delegate = self
+        scrollView.maximumZoomScale = CGFloat.infinity
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +47,26 @@ class AttachmentViewerViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    // MARK: Scroll View
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imgView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        updateContraintForView(view.bounds.size)
+    }
+    
+    func updateContraintForView(size: CGSize){
+        let yOffset = max(0, (size.height - imgView.frame.height)/2)
+        let xOffset = max(0, (size.width - imgView.bounds.width)/2)
+        
+        imgViewTopConstraint.constant = yOffset
+        imgViewBottomConstraint.constant = yOffset
+        
+        imgViewLeadingConstraint.constant = xOffset
+        imgViewTrailingConstraint.constant = xOffset
+        
+        view.layoutIfNeeded()
+    }
 }
